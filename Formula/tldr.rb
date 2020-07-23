988d8ab2aa8ca40c265499e74d5bf3987bb407d7
Sami Khan <eiladin@gmail.com>
2020-07-23 11:43:18 -0500

    Brew formula update for tldr version v1.4.6

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -2,16 +2,16 @@
 class Tldr < Formula
   desc "Simplified and community-driven man pages"
   homepage "https://github.com/eiladin/tldr"
-  version "1.4.5"
+  version "1.4.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/eiladin/tldr/releases/download/v1.4.5/tldr_1.4.5_macos_x64.tar.gz"
-    sha256 "0164911d36d88f6ef843cbb7618c4955e6f062e7d5ebdaca4267c282a5e1a646"
+    url "https://github.com/eiladin/tldr/releases/download/v1.4.6/tldr_1.4.6_macos_x64.tar.gz"
+    sha256 "50775fa75c6bc85a75e8585ccd9ca783928c957181b661c8f3050e33a6c2850e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/eiladin/tldr/releases/download/v1.4.5/tldr_1.4.5_linux_x64.tar.gz"
-      sha256 "e74f3be0ab1b5168bfeb317470ddfafeb91d839ffa54fb91dec66cd042ccd95f"
+      url "https://github.com/eiladin/tldr/releases/download/v1.4.6/tldr_1.4.6_linux_x64.tar.gz"
+      sha256 "2f5088a361ac463f160e6f6df2db269811ad60530d60e272ab2e8ad6450d1ee1"
     end
   end
 
